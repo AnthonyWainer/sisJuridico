4501672cc363cb6c3a8542fff8597c6db1ba0bbb
anthonywainer <guivin95@gmail.com>
2015-05-16 15:46:43 -0500

    nuevo

--- sisjuridico/sisjuridico/urls.py
+++ sisjuridico/sisjuridico/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 
 urlpatterns = [ 
@@ -7,5 +8,6 @@ urlpatterns = [
     # url(r'^$', 'clase1.views.home', name='home'),
     url(r'^', include('apps.app.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
     
 ]
