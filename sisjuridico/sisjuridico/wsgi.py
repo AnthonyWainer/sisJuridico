2941290f0503ba9bde3c72780aaa97df837794d4
anthonywainer <guivin95@gmail.com>
2015-06-05 16:05:03 -0500

    password

--- sisjuridico/sisjuridico/wsgi.py
+++ sisjuridico/sisjuridico/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sisjuridico.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sisjuridico.settings.production")
 
 #application = get_wsgi_application()
 
