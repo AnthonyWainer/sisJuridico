64ab23820ae3cac248941a95c9e7e56508ec0f95
anthonywainer <guivin95@gmail.com>
2015-06-20 15:02:09 -0500

    bk

--- sisjuridico/sisjuridico/wsgi.py
+++ sisjuridico/sisjuridico/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sisjuridico.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sisjuridico.settings.local")
 
 application = get_wsgi_application()
 
