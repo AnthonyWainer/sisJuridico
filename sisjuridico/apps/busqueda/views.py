8a161cd62b2dd3fcd8aecd8c1d5497d5f4cc73aa
anthonywainer <guivin95@gmail.com>
2015-07-04 09:28:57 -0500

    everything mantenimiento

--- sisjuridico/apps/busqueda/views.py
+++ sisjuridico/apps/busqueda/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.db.models import Max, Q
+from apps.paginacion import paginacion
 from apps.expediente.models import categoria, expedientes, resolucion
 from apps.seguridad.models import permisos
-
 import datetime
 today  = datetime.datetime.now()
 fecha   = today.strftime("%Y-%m-%d") 
@@ -30,7 +30,7 @@ def busqueda_especifica(request):
         idc= 1
         expediente = expedientes.objects.filter(idcategoria= idc).order_by('id')
         modulo = {'lista':expediente, 'url':'busqueda_especifica/','n':'busquedaU','estado':estado,'idcategoria':listaCategoria, 'idresolucion':listaResolucion,'fecha' :fecha}
-        return render(request,  'busqueda/busqueda_especifica.html',modulo )        
+        return paginacion(request,expediente, modulo, 'busqueda/busqueda_especifica.html' )        
 
 def busqueda(request):
     e = expedientes.objects.filter( Q(nro__contains=request.POST["nro"]), Q(idcategoria__id__contains=request.POST["idcategoria"]), Q(fecha__contains=request.POST["fecha"]) )[:10]
